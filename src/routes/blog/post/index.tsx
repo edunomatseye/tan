d0d8f9a5474b10ba8581211d589c015ef4af1807
Edun Omatseye <edunomatseye@yahoo.com>
2025-01-09 22:56:14 +0100

    chore(core): use loaderDeps and loaders in route

--- src/routes/blog/post/index.tsx
+++ src/routes/blog/post/index.tsx
@@ -10,11 +10,19 @@ const productSearchSchema = z.object({
   page: z.number().catch(1),
   filter: z.string().catch(""),
   sort: z.enum(["newest", "oldest", "price"]).catch("newest"),
+  offset: z.string(),
+  limit: z.number(),
 });
 
 export const Route = createFileRoute("/blog/post/")({
   component: RouteComponent,
   validateSearch: productSearchSchema,
+  loaderDeps: ({ search: { offset, limit } }) => ({ offset, limit }),
+  loader: ({ deps: { offset, limit } }) =>
+    fetchPosts({
+      offset,
+      limit,
+    }),
 });
 
 function RouteComponent() {
